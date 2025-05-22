deee307bf3c504989c3393dabcc8d51fa1c35ce6
Robert Gonzalez <robert.f.r.gonzalez@gmail.com>
2025-05-22 14:33:58 -0700

    Adding Array and ArrayList Descriptions + Adding the Codes.

---
+++ Difference.java
@@ -0,0 +1,92 @@
+/**
+Using the same code for the Array and ArrayList codes.
+Demonstrates the difference in approach while executing the same task.
+Last section showing an example of what an ArrayList can do, which an Array cannot.
+*/
+
+import java.util.ArrayList;
+
+public class Difference
+{
+public static void main(String[] args)
+    {
+        /**
+         Reads elements from an array and prints the largest and the smallest.
+         */
+        int[] inputs = {300, 600, 435, 60348, 85430, 212, 2}; // All elements are declared here. Note: cannot add more elements after this line.
+        int largest = inputs[0]; // Establish the first element as the largest.
+        int smallest = inputs[0]; // Establish the first element as the smallest.
+
+        for (int a = 0; a < inputs.length; a++) // Goes through each element to locate the largest and smallest elements until the end of the array.
+        {
+            if (inputs[a] > largest)
+            {
+                largest = inputs[a];
+            }
+
+            if (inputs[a] < smallest)
+            {
+                smallest = inputs[a];
+            }
+        }
+
+        System.out.println(); // Adding spaces for cleaner output.
+        System.out.println("Using an Array:\n" + "The largest number is " + largest); // Demonstrating the code works.
+        System.out.println("The smallest number is " + smallest); // Demonstrating the code works.
+        System.out.println(); // Adding spaces for cleaner output.
+
+        /**
+         Reads elements from an ArrayList and prints the largest and the smallest.
+         */
+        ArrayList<Integer> inputList = new ArrayList<>(); // Declare and initialize the ArrayList, but nothing is in there yet. 
+        inputList.add(300); // Add every value into the ArrayList. Instead of adding during the declaration, you have to use .add function.
+        inputList.add(600); // Could add more values after the code as well, which is below.
+        inputList.add(435);
+        inputList.add(60348);
+        inputList.add(85430);
+        inputList.add(212);
+        inputList.add(2);
+
+        int largestList = inputList.get(0); // Initialize largest value with zero, using the .get.
+        int smallestList = inputList.get(0); // Initialize largest value with zero, using the .get.
+        int counter = 0;
+        for (int a = 0; a < inputList.size(); a++) // Instead of .length, you use .size.
+        {
+            if (inputList.get(a) > largestList) // Instead of using "[]", you use .get to grab the value from the ArrayList while the loop executes.
+            {
+                largestList = inputList.get(a);
+            }
+
+            if (inputList.get(a) < smallestList)
+            {
+                smallestList = inputList.get(a);
+            }
+            counter++;
+        }
+        System.out.println("Using an ArrayList:\n" + "The largest number is " + largestList); // Demonstrating the code works.
+        System.out.println("The smallest number is " + smallestList); // Demonstrating the code works.
+        System.out.println(); // Adding spaces for cleaner output.
+
+        inputList.add(22593); // Adding two more values to the ArrayList to demonstate the ability to add after declaration.
+        inputList.add(1000000);
+
+        System.out.println("Here is an example of being able to add more values to the ArrayList after declaration: ");
+
+        int c = 1;
+
+        for (int b = 0; b < inputList.size(); b++) // Printing every value within the ArrayList to show what lives in there.
+        {
+            if (b < counter)
+            {
+                System.out.println(c + ". " + inputList.get(b));
+                c++;
+            }
+            else
+            {
+                System.out.println(c + ". " + inputList.get(b) + " (Added after declaration)"); // Printing the last values added after declaration.
+                c++;
+            }
+        }
+        System.out.println(); // Adding spaces for cleaner output.
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# CSC-262-Discussion-3-RGCSC-262-Discussion-3-RG
+# CSC-262-Discussion-3-RG
+Array
+
+An Array in JavaScript is a collection of values with the same data type. It is essentially a storage location with values given or written to it. The benefit of an array is that, instead of collecting and storing values within a new variable for many inputs, you can add them to an array and summon the specific input easily. Opting to use an Array saves time and code length, but most importantly, avoids repetitive code. You cannot store strings, integers, and doubles into one array, so use the proper organization and make a separate array depending on your specific needs. The downside of an array (depending on how you look at it) is that Arrays are size-limited, so once declared, you cannot extend the boundary.
+
+ArrayList
+
+An ArrayList is nearly identical to a Vector in C++ (if anyone is familiar with those) and almost shares the same basis as an Array, but with some more beneficial aspects. Identical to an Array, an ArrayList holds values read from or given by a user and stores those within itself. Unlike its counterpart, it can grow and shrink, regardless of its size during declaration. Which makes it more flexible and user-friendly, but does involve having to manage it a bit more. Unfortunately, an ArrayList cannot be declared as an int or a double type due to those being primitive types. However, it can be an Integer or Double type since those are from the wrapper class (which is seen as an object).
+
+Difference between Array and ArrayList
+
+There are a handful of differences between an Array and an ArrayList, but the biggest is flexibility. When an array is declared as ten, it only holds ten values, cannot be adjusted, or will be out of bounds. A workaround could be adjusting the boundary at the declaration, but it's insufficient and can lead to problems in larger code. An ArrayList helps keep the code clean and easier to manage over time since it can grow and shrink as needed. Another difference is that an Array can store primitive type values such as int, double, char, etc, and objects. However, an ArrayList is limited only to objects, so to hold primitive types in an ArrayList, the programmer must convert the objects to receive the same workflow as an Array. Doing this does lead to extra work.
